cc76038ff567861a3d6573caefccc5617abae6af
Aidan Margo <aidanmargo@gmail.com>
2021-09-21 11:58:21 -0500

    commit tree 730d36ec4ea92708d45b1ebb92352d787ed71961

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return (customerName.toUpperCase());
+}
+
+const setBestCustomer = () => { let bestCustomer = 'not bob'; }
+
+const overwriteBestCustomer = () => bestCustomer = 'Maureen';
+
+const leastFavoriteCustomer = 'Karen'
+
+const changeLeastFavoriteCustomer = () => leastFavoriteCustomer = 'Jacqueline'
